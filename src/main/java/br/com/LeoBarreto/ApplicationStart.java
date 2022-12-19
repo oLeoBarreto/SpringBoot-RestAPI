519a0dff6532a1bbbd7a30f86e944bfc18722050
oLeoBarreto <leobcontato@gmail.com>
2022-12-18 22:47:28 -0300

    Adicionando rate limiter com bucket4j e Hazelcast

--- src/main/java/br/com/LeoBarreto/ApplicationStart.java
+++ src/main/java/br/com/LeoBarreto/ApplicationStart.java
@@ -2,8 +2,10 @@ package br.com.LeoBarreto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApplicationStart {
 
 	public static void main(String[] args) {
